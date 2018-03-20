7cf3d7ac3d1671586fcf640fda370a7948dd53a3
PhoenixBoisnier <pboisnie@oswego.edu>
2018-03-20 13:53:53 -0400

    Added exception to git repo.

---
+++ src/main/java/com/FoodTracker/AddFoodException.java
@@ -0,0 +1,17 @@
+package main.java.com.FoodTracker;
+
+/**
+ * 
+ * Super simple exception made for AddFood in FoodTrackerApp.
+ * 
+ * @author phoenix
+ *
+ */
+public class AddFoodException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7780352002298622366L;
+
+}
