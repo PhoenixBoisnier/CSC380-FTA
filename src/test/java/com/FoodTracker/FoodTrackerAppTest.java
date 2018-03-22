f5b386f457622e9574de606a339cd403820b1236
PhoenixBoisnier <pboisnie@oswego.edu>
2018-03-21 22:17:59 -0400

    Re-ran mvn test after clean.

---
+++ src/test/java/com/FoodTracker/FoodTrackerAppTest.java
@@ -0,0 +1,24 @@
+package test.java.com.FoodTracker;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import main.java.com.FoodTracker.FoodTrackerApp;
+
+public class FoodTrackerAppTest {
+	
+	FoodTrackerApp testApp = new FoodTrackerApp();
+
+	@Test public void addItTest() {
+		
+		
+		
+	}
+	
+	@Test public void makeFoodTest() {
+		
+		
+		
+	}
+}
