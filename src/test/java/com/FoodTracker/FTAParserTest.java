92ff99d29e24cbd4b39581ece76dca5e3a98b7de
PhoenixBoisnier <pboisnie@oswego.edu>
2018-04-17 13:56:43 -0400

    Added parserTest to repository.

---
+++ src/test/java/com/FoodTracker/FTAParserTest.java
@@ -0,0 +1,15 @@
+package test.java.com.FoodTracker;
+
+import org.junit.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import main.java.com.FoodTracker.*;
+
+@SuppressWarnings("unused")
+public class FTAParserTest {
+	  @Test public void testName() {
+		  //TODO make classes
+		  //TODO use methods from classes
+		  //TODO assertTrue(condition)
+	  }
+	}
